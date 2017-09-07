fd360b6729a4a0c74739a807460ee697b89ff8d9
Dmitry <lbvf50.mobile@gmail.com>
2017-09-07 12:41:07 +0700

    pause

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,2 +1,11 @@
 class Bottels
+    def verse(number)
+        verse_for(number).text
+    end
+    def verse_for(number)
+        case number
+    when 0 then Verse.new(number, &NoMore)
+    else Verse.new(number, &Default)
+        end
+    end
 end
