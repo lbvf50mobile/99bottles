d535b7bd55a5c84889f0b83c96e7f3843d4b5ef8
Dmitry <lbvf50.mobile@gmail.com>
2017-09-08 12:35:32 +0700

    Add some tools and fix rubocop problem

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,11 @@
 # Speculatively General
-class Bottels
+class Bottles
+  Default = lambda do |verse|
+    "#{verse.number} bottles of beer on the wall,\
+#{verse.number} bottles of beer.\n \
+Take one down and pass it around,\
+#{verse.number - 1} bottles of beer on the wall.\n"
+  end
   def verse(number)
     verse_for(number).text
   end
@@ -9,5 +15,18 @@ class Bottels
     when 0 then Verse.new(number, &NoMore)
     else Verse.new(number, &Default)
     end
+  end
+end
+
+# Class descripe one verse from the song
+class Verse
+  attr_reader :number
+  def initialize(number, &lyrics)
+    @number = number
+    @lyrics = lyrics
+  end
+
+  def text
+    @lyrics.call self
   end
 end
---
+++ r.sh
@@ -0,0 +1,1 @@
+rubocop lib/bottles.rb
